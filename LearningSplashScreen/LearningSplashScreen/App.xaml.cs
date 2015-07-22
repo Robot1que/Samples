568c21c44fde86ebb171fd5f3490aae322b83c74
Pavels Ahmadulins <pavels.ahmadulins@privax.com>
2015-07-22 12:20:48 +0100

    Splash screen example is uploaded.

---
+++ LearningSplashScreen/LearningSplashScreen/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LearningSplashScreen
+{
+    public partial class App : Application
+    {
+        private const int SplashScreenMinimumDelayMiliseconds = 1500;
+    
+        private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var splashScreen = new SplashScreen();
+            splashScreen.Show();
+            
+            var splashScreenDelayTask = Task.Delay(App.SplashScreenMinimumDelayMiliseconds);
+
+            var mainWindow = new MainWindow();
+            
+            await 
+                Task.WhenAll(
+                    mainWindow.RunLongProcess(),
+                    splashScreenDelayTask
+                );
+
+            splashScreen.Close();
+            mainWindow.Show();
+        }
+    }
+}
